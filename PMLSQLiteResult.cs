388be2c39fbb428a3070f2577060f6549afd3cd4
K.Awata <69828489+k-awata@users.noreply.github.com>
2023-04-13 14:59:27 +0900

    Remove unnecessary usings

--- PMLSQLiteResult.cs
+++ PMLSQLiteResult.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Data.SQLite;
 using Aveva.Core.PMLNet;
 
--- PMLSQLiteStatement.cs
+++ PMLSQLiteStatement.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Data.SQLite;
+﻿using System.Data.SQLite;
 using Aveva.Core.PMLNet;
 
 namespace PMLSQLite
